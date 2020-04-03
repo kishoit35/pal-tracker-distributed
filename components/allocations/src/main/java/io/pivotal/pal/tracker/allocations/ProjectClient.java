f0d4d51d29fba9d8279b19add42629d424719ed2
kishoit35 <kisho.it35@gmail.com>
2020-04-03 20:58:02 +0530

    Securing a Distributed System

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -12,20 +12,20 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ProjectClient {
 
     private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+    private final String endpoint;
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.endpoint = registrationServerEndpoint;
     }
 
     @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
         //return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-        ProjectInfo project = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
         projectsCache.put(projectId, project);
         return project;
     }
